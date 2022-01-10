5b2e9d348987ac803b660d8a30cb8d8ae796e3a6
Lotus 98T <rpitty@live.com>
2022-01-11 08:48:58 +1000

    Select image using react state (only a test).

--- src/Gallery.js
+++ src/Gallery.js
@@ -9,8 +9,17 @@ import product2Thumbnail from "./images/image-product-2-thumbnail.jpg";
 import product3Thumbnail from "./images/image-product-3-thumbnail.jpg";
 import product4Thumbnail from "./images/image-product-4-thumbnail.jpg";
 import "./Gallery.css";
+import { useState } from "react";
 
 const Gallery = () => {
+    const [selectedImage, setSelectedImage] = useState(product1);
+
+    const changeSelectedImage = (e, image) => {
+        setSelectedImage(image);
+        document.querySelector(".selected").classList.remove("selected");
+        e.target.parentElement.classList.add("selected");
+    }
+
     return (
         <section className="Gallery">
             <div className="Gallery__selected-image">
@@ -20,20 +29,20 @@ const Gallery = () => {
                 <div className="Gallery__selector-arrow-right">
                     <img src={right} alt="right-arrow" />
                 </div>
-                <img src={product1} alt="selected" />
+                <img src={selectedImage} alt="selected" />
             </div>
             <div className="Gallery__selector-wrapper">
                 <div className="Gallery__selector-container">
-                    <div className="Gallery__selector selected">
+                    <div className="Gallery__selector selected" onClick={(e) => changeSelectedImage(e, product1)}>
                         <img src={product1Thumbnail} alt="selector1" />
                     </div>
-                    <div className="Gallery__selector">
+                    <div className="Gallery__selector" onClick={(e) => changeSelectedImage(e, product2)}>
                         <img src={product2Thumbnail} alt="selector2" />
                     </div>
-                    <div className="Gallery__selector">
+                    <div className="Gallery__selector" onClick={(e) => changeSelectedImage(e, product3)}>
                         <img src={product3Thumbnail} alt="selector3" />
                     </div>
-                    <div className="Gallery__selector">
+                    <div className="Gallery__selector" onClick={(e) => changeSelectedImage(e, product4)}>
                         <img src={product4Thumbnail} alt="selector4" />
                     </div>
                 </div>
