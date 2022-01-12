85973a9bf7d6153d9682e7977a709d2806c5aeea
Lotus 98T <rpitty@live.com>
2022-01-12 16:56:18 +1000

    Added selected product quantity state (input).

--- src/App.js
+++ src/App.js
@@ -5,8 +5,14 @@ import Description from './Description';
 import Gallery from './Gallery';
 import Navbar from './Navbar';
 import Quantity from './Quantity';
+import { useState } from 'react/cjs/react.development';
 
 function App() {
+  // Manages the currently selected product quantity state (user input)
+  const [currentQuantity, setCurrentQuantity] = useState(0);
+
+  const updateQuantity = (value) => setCurrentQuantity(value);
+
   return (
     <div className="App">
       <Navbar />
@@ -16,7 +22,7 @@ function App() {
         </div>
         <div className="container__column2">
           <Description />
-          <Quantity />
+          <Quantity currentQuantity={ currentQuantity } updateQuantity={ updateQuantity } />
         </div>
       </div>
       <Attribution />
--- src/Quantity.js
+++ src/Quantity.js
@@ -3,23 +3,23 @@ import plus from "./images/icon-plus.svg";
 import cart from "./images/icon-cart.svg";
 import "./Quantity.css";
 
-const Quantity = () => {
+const Quantity = ({ currentQuantity, updateQuantity }) => {
     return (
         <section className="Quantity">
             <div className="Quantity__selector">
-                <div className="Quantity__selector-minus">
+                <div className="Quantity__selector-minus" onClick={() => currentQuantity > 0 && updateQuantity(currentQuantity - 1)}>
                     <img src={minus} alt="minus" />
                 </div>
                 <div className="Quantity__selector-value">
-                    <p>0</p>
+                    <p>{ currentQuantity }</p>
                 </div>
-                <div className="Quantity__selector-plus">
-                    <img src={plus} alt="plus" />
+                <div className="Quantity__selector-plus" onClick={() => updateQuantity(currentQuantity + 1)}>
+                    <img src={ plus } alt="plus" />
                 </div>
             </div>
             <div className="Quantity__cart">
                 <div className="Quantity__cart-icon">
-                    <img src={cart} alt="cart icon" />
+                    <img src={ cart } alt="cart icon" />
                 </div>
                 <div className="Quantity__cart-text">
                     <p>Add to cart</p>
