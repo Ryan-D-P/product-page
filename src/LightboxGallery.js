d6afd36a420bfa63538faf56dd55a5e7b73124ca
Lotus 98T <rpitty@live.com>
2022-01-18 18:32:59 +1000

    Correct image displayed when lightbox is opened.

--- src/LightboxGallery.js
+++ src/LightboxGallery.js
@@ -2,10 +2,11 @@ import { useEffect } from "react";
 import close from "./images/icon-close.svg";
 import "./LightboxGallery.css";
 
-const LightboxGallery = ({ setLightbox, arrowImages, changeSelectedByArrow, changeSelected, left, right, product1, product2, product3, product4, product1Thumbnail, product2Thumbnail, product3Thumbnail, product4Thumbnail }) => {
+const LightboxGallery = ({ setLightbox, imageIndex, changeSelectedByArrow, changeSelected, left, right, product1, product2, product3, product4, product1Thumbnail, product2Thumbnail, product3Thumbnail, product4Thumbnail }) => {
     // Run function when lightbox is opened to get the current image index
     useEffect(() => {
-        console.log(" ");
+        document.querySelectorAll(`.LightboxGallery__image`)[imageIndex].classList.add("show-selected");
+        document.querySelectorAll(`.LightboxGallery__selector`)[imageIndex].classList.add("selected");
     }, []);
 
     return (
@@ -18,30 +19,30 @@ const LightboxGallery = ({ setLightbox, arrowImages, changeSelectedByArrow, chan
                 </div>
                 <div className="LightboxGallery__flex-row LightboxGallery__selected-image-row">
                     <div className="LightboxGallery__selected-image">
-                        <div className="LightboxGallery__selector-arrow-left hidden-arrow" onClick={ (e) => changeSelectedByArrow(e, "LightboxGallery") } data-direction={ "left" }>
+                        <div className={ `LightboxGallery__selector-arrow-left ${imageIndex === 0 && "hidden-arrow"}` } onClick={ (e) => changeSelectedByArrow(e, "LightboxGallery") } data-direction={ "left" }>
                             <img src={ left } alt="left-arrow" data-direction={ "left" } />
                         </div>
-                        <div className="LightboxGallery__selector-arrow-right" onClick={ (e) => changeSelectedByArrow(e, "LightboxGallery") } data-direction={ "right" }>
+                        <div className={ `LightboxGallery__selector-arrow-right ${imageIndex === 3 && "hidden-arrow"}` } onClick={ (e) => changeSelectedByArrow(e, "LightboxGallery") } data-direction={ "right" }>
                             <img src={ right } alt="right-arrow" data-direction={ "right" } />
                         </div>
-                        <img src={ product1 } alt="selected-image1" className="LightboxGallery__image show-selected" data-image-index={ "image1" } />
-                        <img src={ product2 } alt="selected-image2" className="LightboxGallery__image" data-image-index={ "image2" } />
-                        <img src={ product3 } alt="selected-image3" className="LightboxGallery__image" data-image-index={ "image3" } />
-                        <img src={ product4 } alt="selected-image4" className="LightboxGallery__image" data-image-index={ "image4" } />
+                        <img src={ product1 } alt="selected-image1" className="LightboxGallery__image" data-image-index={ "0" } />
+                        <img src={ product2 } alt="selected-image2" className="LightboxGallery__image" data-image-index={ "1" } />
+                        <img src={ product3 } alt="selected-image3" className="LightboxGallery__image" data-image-index={ "2" } />
+                        <img src={ product4 } alt="selected-image4" className="LightboxGallery__image" data-image-index={ "3" } />
                     </div>
                 </div>
                 <div className="LightboxGallery__flex-row LightboxGallery__gallery-selector-row">
                     <div className="LightboxGallery__selector-container">
-                        <div className="LightboxGallery__selector selected" onClick={ () => changeSelected("image1", "LightboxGallery") } data-image-index={ "image1" }>
+                        <div className="LightboxGallery__selector" onClick={ () => changeSelected("0", "LightboxGallery") } data-image-index={ "0" }>
                             <img src={ product1Thumbnail } alt="product 1" />
                         </div>
-                        <div className="LightboxGallery__selector" onClick={ () => changeSelected("image2", "LightboxGallery") } data-image-index={ "image2" }>
+                        <div className="LightboxGallery__selector" onClick={ () => changeSelected("1", "LightboxGallery") } data-image-index={ "1" }>
                             <img src={ product2Thumbnail } alt="product 2" />
                         </div>
-                        <div className="LightboxGallery__selector" onClick={ () => changeSelected("image3", "LightboxGallery") } data-image-index={ "image3" }>
+                        <div className="LightboxGallery__selector" onClick={ () => changeSelected("2", "LightboxGallery") } data-image-index={ "2" }>
                             <img src={ product3Thumbnail } alt="product 3" />
                         </div>
-                        <div className="LightboxGallery__selector" onClick={ () => changeSelected("image4", "LightboxGallery") } data-image-index={ "image4" }>
+                        <div className="LightboxGallery__selector" onClick={ () => changeSelected("3", "LightboxGallery") } data-image-index={ "3" }>
                             <img src={ product4Thumbnail } alt="product 4" />
                         </div>
                     </div>
